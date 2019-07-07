f60c0d97a97aefeb161337467589e42d1a3a5706
dk <113>
2019-07-07 13:58:17 +0800

    first commit

---
+++ src/main/java/com/dk/bbs/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.dk.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author : dk
+ * @date : 2019/7/7 11:27
+ * @description :
+ */
+@Controller
+public class Hello {
+
+}
